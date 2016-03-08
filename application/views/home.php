b76788b715215eb8102eb0a5a8dfbd523ea8c4b4
etiennegithub <etienne.gobion@ynov.com>
2016-03-08 16:09:41 +0100

    Début accueil

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,19 @@
-<?php
+<div id="slogant"><h1>Super voisin : une solution pragmatique aux problèmes du quotidien</h1></div>
+
+<div id="recherche" >
+    <form action="#" method="post">
+        <div id="AceuilQuestionnaire">
+            <div class="col-md-offset-2 col-md-4" size="40">	
+                <input type="text" placeholder="Où vous trouver vous ?"  class="form-control" >
+            </div>
+            <div class="col-md-offset-0 col-md-4" size="40">
+                <input type="text"  placeholder="Que rechercher vous ?" class="form-control" >
+            </div>
+            <div id="boutonRecherche">
+                <div class="col-md-offset-4 col-md-4">
+                    <button type="submit" class="btn btn-info">Rechercher</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
